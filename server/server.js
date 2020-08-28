b9288dc2e764baf7905864ce2ccba46d00eb329c
Julio Cesar Ramirez Jimenez <julio_marshal@hotmail.com>
2020-08-27 20:51:50 -0500

    added POST PUT and GET

--- server/server.js
+++ server/server.js
@@ -1,37 +1,21 @@
 require("./config/config");
+
 const express = require("express");
+const mongoose = require("mongoose");
+const colors = require("colors");
+
 const app = express();
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
-
-app.get("/usuario", function (req, res) {
-  res.json("get usuario");
-});
-
-app.post("/usuario", function (req, res) {
-  let body = req.body;
-  if (body.nombre === undefined) {
-    res.status(400).json({
-      ok: false,
-      mensaje: "El nombre es necesario",
-    });
-  } else {
-    res.json({ persona: body });
-  }
-});
-
-app.put("/usuario/:id", function (req, res) {
-  let id = req.params.id;
-  res.json({ id });
-});
-
-app.delete("/usuario", function (req, res) {
-  res.json("delete usuario");
+app.use(require("./routes/usuario"));
+mongoose.connect("mongodb://localhost:27017/cafe", (err, res) => {
+  if (err) throw err;
+  console.log("Base de datos ONLINE!".green);
 });
 
 app.listen(process.env.PORT, () => {
-  console.log("Server up!!", process.env.PORT);
+  console.log("Server up!!".green, process.env.PORT.yellow);
 });
