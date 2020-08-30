c004acec31d1c44897b361254f2ab224429f0666
Julio Cesar Ramirez Jimenez <julio_marshal@hotmail.com>
2020-08-29 19:23:42 -0500

    Login

---
+++ server/middlewares/autenticacion.js
@@ -0,0 +1,41 @@
+const jwt = require("jsonwebtoken");
+
+// ====================
+// Verificar token
+// ====================
+
+const verificarToken = (req, res, next) => {
+  const token = req.get("token");
+  jwt.verify(token, process.env.SEED, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({
+        ok: false,
+        err: {
+          message: "Token no válido",
+        },
+      });
+    }
+    req.usuario = decoded.usuario;
+    next();
+  });
+};
+
+// ====================
+// Verifica AdminRole
+// ====================
+const verificaAdminRole = (req, res, next) => {
+  if (req.usuario.role === "ADMIN_ROLE") {
+    next();
+  } else {
+    return res.status(401).json({
+      ok: false,
+      err: {
+        message: "Role no válido",
+      },
+    });
+  }
+};
+module.exports = {
+  verificarToken,
+  verificaAdminRole
+};
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -2,10 +2,14 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const _ = require("underscore");
 const Usuario = require("../models/usuario");
+const {
+  verificarToken,
+  verificaAdminRole,
+} = require("../middlewares/autenticacion");
 
 const app = express();
 
-app.get("/usuario", function (req, res) {
+app.get("/usuario", verificarToken, function (req, res) {
   let desde = req.query.desde || 0;
   desde = Number(desde);
 
@@ -32,7 +36,7 @@ app.get("/usuario", function (req, res) {
     });
 });
 
-app.post("/usuario", function (req, res) {
+app.post("/usuario", [verificarToken, verificaAdminRole], function (req, res) {
   let body = req.body;
   let usuario = new Usuario({
     nombre: body.nombre,
@@ -55,7 +59,10 @@ app.post("/usuario", function (req, res) {
   });
 });
 
-app.put("/usuario/:id", function (req, res) {
+app.put("/usuario/:id", [verificarToken, verificaAdminRole], function (
+  req,
+  res
+) {
   let id = req.params.id;
   let body = _.pick(req.body, ["nombre", "email", "img", "role", "estado"]);
 
@@ -78,7 +85,10 @@ app.put("/usuario/:id", function (req, res) {
   );
 });
 
-app.delete("/usuario/:id", function (req, res) {
+app.delete("/usuario/:id", [verificarToken, verificaAdminRole], function (
+  req,
+  res
+) {
   let id = req.params.id;
   const cambiaEstado = {
     estado: false,
